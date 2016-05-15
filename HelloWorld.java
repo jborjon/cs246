e7a88d5e1cf67d7d65df5e4210e9aa79de61406a
Joseph Borjon <Joseph Borjon>
2016-05-14 22:57:03 -0600

    First commit to the local repo.

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Author: Joseph Borjon
+ * Created: 2016-05-14
+ * Modified: 2016-05-14
+ *
+ */
+
+// Prints 'Hello World' to the console
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
